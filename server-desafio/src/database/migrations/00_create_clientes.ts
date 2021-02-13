7a80340ba5859c0b4319208fb94b322d401c865e
Margotti3 <mmargottia@gmail.com>
2021-02-13 15:44:44 -0300

    edit databease

--- server-desafio/src/database/migrations/00_create_clientes.ts
+++ server-desafio/src/database/migrations/00_create_clientes.ts
@@ -3,13 +3,13 @@ import Knex from 'knex';
 export async function up(knex: Knex){
     return knex.schema.createTable('clientes', table => {
         table.increments('id').primary();
-        table.integer('CNPJ').notNullable();
+        table.integer('CNPJ', 14).notNullable();
         table.string('nome').notNullable();
         table.date('data_fundacao').notNullable();
         table.string('tipo', 7).notNullable();
-        table.integer('telefone').notNullable();
+        table.integer('telefone', 15).notNullable();
         table.string('email').notNullable();
-        table.integer('CEP').notNullable();
+        table.integer('CEP', 8).notNullable();
         table.string('logradouro').notNullable();
         table.integer('numero').notNullable();
         table.string('bairro').notNullable();
--- server-desafio/src/database/migrations/01_create_pessoasR.ts
+++ server-desafio/src/database/migrations/01_create_pessoasR.ts
@@ -6,7 +6,7 @@ export async function up(knex: Knex){
         table.integer('CPF').notNullable();
         table.string('nome').notNullable();
         table.date('data_nascimento').notNullable();
-        table.integer('telefone').notNullable();
+        table.integer('telefone', 15).notNullable();
         table.string('email').notNullable();
         table.integer('id_cliente', 10).unsigned().notNullable();
         table.foreign('id_cliente')
